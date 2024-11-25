6689a6ecfcb98a8a6afbcf8142130a98d5788d9e
RenanKr4t0s <redrums88@gmail.com>
2024-11-25 16:50:15 -0300

    corrections ports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ejs from "ejs"
 import characters from '../assets/characters.js';
 
 const app = express();
-const PORT = 4000
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
